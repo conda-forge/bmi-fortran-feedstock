f6df713a6dae650896c8b6c6d3d7dcac3e11ffb4
Mark Piper <mark.piper@colorado.edu>
2019-08-27 15:53:58 -0600

    Change install location

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ mkdir _build
 cd _build
 
 cmake -G "NMake Makefiles" ^
-  -DCMAKE_INSTALL_PREFIX:PATH=%PREFIX% ^
+  -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
   -DCMAKE_BUILD_TYPE=Release ^
   %SRC_DIR%
 
